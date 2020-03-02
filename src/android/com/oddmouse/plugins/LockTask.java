ab79914bb3343a8ed8b35da6f3a6001be99834ed
Antoine Decriem <a.decriem@codecare.fr>
2020-03-02 18:08:13 +0100

    update

--- src/android/com/oddmouse/plugins/LockTask.java
+++ src/android/com/oddmouse/plugins/LockTask.java
@@ -5,6 +5,7 @@ import android.app.ActivityManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.view.View;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -44,6 +45,16 @@ public class LockTask extends CordovaPlugin {
             if (mDPM.isDeviceOwnerApp(activity.getPackageName())) {
               String[] packages = {activity.getPackageName()};
               mDPM.setLockTaskPackages(mDeviceAdmin, packages);
+			  
+			  activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+				activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			  activity.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             }
 
           }
