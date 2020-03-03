2bc493c7ac54e69a1978dda55f0056424bc863e8
Antoine Decriem <a.decriem@codecare.fr>
2020-03-03 09:45:11 +0100

    ajout des fichiers kiosk plugin

--- src/android/com/oddmouse/plugins/LockTask.java
+++ src/android/com/oddmouse/plugins/LockTask.java
@@ -9,6 +9,8 @@ import android.view.View;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -26,12 +28,6 @@ public class LockTask extends CordovaPlugin {
     ActivityManager activityManager = (ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE);
     String adminClassName = "com.mama.deviceadmin.CordovaDeviceAdminReceiver";
 
-	/*
-    if (args.length() > 0) {
-      adminClassName = args.getString(0);
-    }
-	*/
-
     try {
       if (ACTION_START_LOCK_TASK.equals(action)) {
 
@@ -45,18 +41,6 @@ public class LockTask extends CordovaPlugin {
             if (mDPM.isDeviceOwnerApp(activity.getPackageName())) {
               String[] packages = {activity.getPackageName()};
               mDPM.setLockTaskPackages(mDeviceAdmin, packages);
-			  
-			  /*
-			  activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-				activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-			  activity.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                  | View.SYSTEM_UI_FLAG_FULLSCREEN
-                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-				  */
             }
 
           }
