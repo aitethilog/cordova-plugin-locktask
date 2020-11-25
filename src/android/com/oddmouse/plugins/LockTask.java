73e0da95dc2233234b697a9910cf63197fdff2b9
Elian Fulachier <e.fulachier@codecare.fr>
2020-11-25 17:07:46 +0100

    1.0.2 -> 1.0.3 suppression des fonctions launcher

--- src/android/com/oddmouse/plugins/LockTask.java
+++ src/android/com/oddmouse/plugins/LockTask.java
@@ -26,11 +26,6 @@ public class LockTask extends CordovaPlugin {
   private static final String ACTION_IS_IN_KIOSK = "isInKiosk";
   private static final String ACTION_REMOVE_DEVICE_OWNER = "removeDeviceOwner";
 
-  public static final String IS_SET_AS_LAUNCHER = "isSetAsLauncher";
-  public static final String EXIT_LAUNCHER = "exitLauncher";
-  public static final String SELECT_LAUNCHER = "selectLauncher";
-  public static final String REMOVE_LAUNCHER = "removeLauncher";
-
   private Activity activity = null;
 
   @Override
@@ -64,53 +59,7 @@ public class LockTask extends CordovaPlugin {
         callbackContext.success();
         return true;
 
-      } else if (IS_SET_AS_LAUNCHER.equals(action)) {
-        callbackContext.success(Boolean.toString(isSetAsDefaultLauncher()));
-        return true;
-
-      } else if (EXIT_LAUNCHER.equals(action)) {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-		intent.addCategory(Intent.CATEGORY_HOME);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		
-		Intent chooser = Intent.createChooser(intent, "Select destination...");
-		if (intent.resolveActivity(cordova.getActivity().getPackageManager()) != null) {
-			cordova.getActivity().startActivity(chooser);
-		}
-		
-		callbackContext.success();
-		return true;
-
-      } else if (SELECT_LAUNCHER.equals(action)) {
-        Intent startMain = new Intent(Intent.ACTION_MAIN);
-		startMain.addCategory(Intent.CATEGORY_HOME);
-		startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		cordova.getActivity().startActivity(startMain);
-		
-		callbackContext.success();
-		return true;
-
-      } else if (REMOVE_LAUNCHER.equals(action)) {
-		activity = cordova.getActivity();
-		PackageManager pkgMngr = activity.getPackageManager();
-		String packageName = activity.getPackageName();
-		
-		if (isSetAsDefaultLauncher()) {
-			pkgMngr.clearPackagePreferredActivities(activity.getPackageName());
-		}
-		else {
-			ComponentName cn1 = new ComponentName(packageName, packageName + ".LauncherAlias1");
-			ComponentName cn2 = new ComponentName(packageName, packageName + ".LauncherAlias2");
-			int dis = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-			if(pkgMngr.getComponentEnabledSetting(cn1) == dis) dis = 3 - dis;
-			pkgMngr.setComponentEnabledSetting(cn1, dis, PackageManager.DONT_KILL_APP);
-			pkgMngr.setComponentEnabledSetting(cn2, 3 - dis, PackageManager.DONT_KILL_APP);
-		}
-		
-		callbackContext.success();
-		return true;
-		
-	  } else {
+      } else {
         callbackContext.error("The method '" + action + "' does not exist.");
         return false;
 
@@ -181,17 +130,5 @@ public class LockTask extends CordovaPlugin {
 
         activity.startLockTask();
       }
-    }
-
-	private Boolean isSetAsDefaultLauncher() {
-		String myPackage = cordova.getActivity().getApplicationContext().getPackageName();
-        return myPackage.equals(findLauncherPackageName());
-	}
-
-    private String findLauncherPackageName() {
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        final ResolveInfo res = this.cordova.getActivity().getPackageManager().resolveActivity(intent, 0);
-        return res.activityInfo.packageName;
     }
 }
--- www/LockTask.js
+++ www/LockTask.js
@@ -17,31 +17,5 @@ module.exports = {
   },
   removeDeviceOwner: function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "LockTask", "removeDeviceOwner", []);
-  },
-  isSetAsLauncher: function (callback) {
-    if(/ios|iphone|ipod|ipad/i.test(navigator.userAgent)) {
-      callback(false); // ios not supported - cannot be in kiosk
-      return;
-    }
-    cordova.exec(function (out) {
-      callback(out == "true");
-    }, function (error) {
-      alert("LockTask.isSetAsLauncher failed: " + error);
-    }, "LockTask", "isSetAsLauncher", []);
-  },
-  exitLauncher: function () {
-	cordova.exec(function () {}, function (error) {
-		alert("LockTask.exitLauncher failed: " + error);
-	}, "LockTask", "exitLauncher", []);
-  },
-  selectLauncher: function () {
-	cordova.exec(function () {}, function (error) {
-		alert("LockTask.selectLauncher failed: " + error);
-	}, "LockTask", "selectLauncher", []);
-  },
-  removeLauncher: function () {
-	cordova.exec(function () {}, function (error) {
-		alert("LockTask.removeLauncher failed: " + error);
-	}, "LockTask", "removeLauncher", []);
   }
 };
